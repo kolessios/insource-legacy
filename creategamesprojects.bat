e96bf93dfcab5f9fbbb8b5fe87a860377340d4be
Kolesias <kolesias17@gmail.com>
2017-11-03 01:31:59 -0600

    VPC Update with small wizard.

--- creategamesprojects.bat
+++ creategamesprojects.bat
@@ -1,3 +1,39 @@
 @echo off
-devtools\bin\vpc.exe +gamedlls /apocalypse /scp /mksln games.sln /2013
+set params=
+
+echo Games
+echo --------
+echo.
+
+set /p Apocalypse= Apocalypse-22? (y): 
+If not "%Apocalypse%"=="n" (
+    set params=%params% /apocalypse
+)
+
+set /p SCP= SCP? (y): 
+If not "%SCP%"=="n" (
+    set params=%params% /scp
+)
+
+echo.
+echo Engine Configuration
+echo --------
+echo.
+
+set /p OpenAL= Use OpenAL? (y): 
+If not "%OpenAL%"=="n" (
+    set params=%params% /define:USE_OPENAL
+)
+
+set /p CEF= Use CEF? (y): 
+If not "%CEF%"=="n" (
+    set params=%params% /define:USE_CEF
+)
+
+set /p L4D2Hacks= Use L4D2 Hacks? (y): 
+If not "%L4D2Hacks%"=="n" (
+    set params=%params% /define:USE_L4D2_HACKS
+)
+
+devtools\bin\vpc.exe +gamedlls %params% /mksln games.sln /2013
 pause
